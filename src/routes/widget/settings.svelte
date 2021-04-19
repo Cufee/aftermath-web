7bfa09735eec99d0be4a598ff4d6f73c5e6884f7
Vovko <vkouzin@outlook.com>
2021-04-19 19:42:16 -0400

    added widget and bot pages

---
+++ src/routes/widget/settings.svelte
@@ -0,0 +1,33 @@
+<script>
+  import Stats from "../../components/widget/stats/Stats.svelte";
+  import { _ } from "svelte-i18n";
+
+  const tmp_request = {};
+</script>
+
+<div class="container">
+  <div class="section">
+    <h3>{$_("nav.widget")}</h3>
+    <Stats />
+  </div>
+  <div class="section">
+    <h3>Settings</h3>
+    <p>Settings</p>
+  </div>
+</div>
+
+<style>
+  .container {
+    display: grid;
+    grid-gap: 1rem;
+    align-items: start;
+    text-align: center;
+    grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+  }
+
+  .section {
+    justify-self: center;
+    align-self: center;
+    height: 400px;
+  }
+</style>
