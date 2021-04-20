0736029c44c591857f4e831fb4b5faca6543b360
Vovko <vkouzin@outlook.com>
2021-04-19 22:18:32 -0400

    added login gif

--- src/routes/login/index.svelte
+++ src/routes/login/index.svelte
@@ -2,4 +2,26 @@
   <title>Aftermath - Login</title>
 </svelte:head>
 
-<div class="container">Coming soon ;)</div>
+<div class="container">
+  <div class="section">Coming soon ;)</div>
+  <div class="section">
+    <img src="./images/Login.gif" alt="Never gonna give you up" />
+  </div>
+</div>
+
+<style>
+  .container {
+    align-content: start;
+    text-align: center;
+    display: grid;
+
+    grid-auto-columns: 1fr;
+    grid-gap: 1rem;
+    height: 100%;
+  }
+
+  .section {
+    justify-self: center;
+    width: 80%;
+  }
+</style>
